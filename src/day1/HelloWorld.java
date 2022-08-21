faf1e81e294ee1cd7927b267f1056b91cbe7fc92
Leyla <cydeo.leyla@gmail.com>
2022-08-20 23:48:20 -0400

    This is new an added

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java World");
         // added
+
+        // when we add always file changes we have to commit
     }
 }
